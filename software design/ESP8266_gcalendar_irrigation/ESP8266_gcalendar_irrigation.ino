a376196a33e7bad6739d00950eee216e259c13a2
cronzon <cronzon@switchfly.com>
2017-07-03 15:53:36 +0000

    Updates Copyright

--- software design/ESP8266_gcalendar_irrigation/ESP8266_gcalendar_irrigation.ino
+++ software design/ESP8266_gcalendar_irrigation/ESP8266_gcalendar_irrigation.ino
@@ -1,14 +1,14 @@
 /*
     Copyright (C) 2017 Clément Ronzon
 
-    This file is part of EspDataLogger.
+    This file is part of GCalendarIrrigation.
 
-    SpotPipe is free software: you can redistribute it and/or modify
+    GCalendarIrrigation is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Foobar is distributed in the hope that it will be useful,
+    GCalendarIrrigation is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
